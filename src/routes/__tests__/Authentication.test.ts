04c4208c8e90459e8eca5cf0e05a17276be4eae0
Rizky Ian Indiarto <rizkyian78@gmail.com>
2021-05-05 11:56:23 +0700

    feat: change to mock testing

--- src/routes/__tests__/Authentication.test.ts
+++ src/routes/__tests__/Authentication.test.ts
@@ -1,69 +1,41 @@
 /* eslint-disable no-undef */
 // @ts-ignore
 import AuthService from 'controllers/Auth/service'
-import User from 'models/User'
-import mongoose from 'mongoose'
+import faker from 'faker'
 
 require('dotenv').config()
 
-let connection
-
-beforeAll(async () => {
-  const USERNAME = process.env.MONGODB_USERNAME_TEST
-  const PASSWORD = process.env.MONGODB_PASSWORD_TEST
-  const AUTH_SOURCE = process.env.MONGODB_AUTH_TEST
-  const HOST = process.env.MONGODB_HOST_TEST
-  const PORT = process.env.MONGODB_PORT_TEST
-  const COLLECTION = process.env.MONGODB_DATABASE_TEST
-
-  const setUri = `mongodb://${HOST}:${PORT}/${COLLECTION}`
-  const setOptions = {
-    user: USERNAME,
-    pass: PASSWORD,
-    authSource: AUTH_SOURCE,
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  }
-
-  connection = () => {
-    // Connecting to the database
-    mongoose
-      .connect(setUri, setOptions)
-      .then(() => {
-        console.log('Successfully connected to the MongoDB database')
-      })
-      .catch((err) => {
-        console.log('Could not connect to the MongoDB database:', err)
-        process.exit()
-      })
-  }
-  connection()
-})
-
-afterAll(async () => {
-  await User.deleteOne({ email: 'any@email.com' })
-  mongoose.disconnect()
-})
+function mockRequest() {
+  return {}
+}
 
 test('Should Successfully create user', async (done) => {
-  const data = await AuthService.signUp({
-    firstName: 'any',
-    email: 'any@email.com',
-    phone: '08113363807',
-    newPassword: 'anyvalue12',
-    confirmNewPassword: 'anyvalue12',
-  })
+  const req: any = mockRequest()
+
+  req.firstName = faker.name.findName()
+  req.email = faker.internet.email()
+  req.phone = faker.phone.phoneNumber()
+  req.newPassword = 'anyvalue12'
+  req.confirmNewPassword = 'anyvalue12'
+
+  const data = await AuthService.signUp(req)
   expect(data).toEqual('user created')
   done()
 })
 
-test('Should Return falsy when random value given', async (done) => {
-  const data = await AuthService.signIn({
-    email: 'any@email.com',
-    password: 'anyvalue12',
-  })
-  console.log(data, '<<<<<<<<<<<< ini data')
+test(`Should successfully generating error when user not exist`, async (done) => {
+  const req: any = mockRequest()
+
+  req.email = faker.internet.email()
+  req.newPassword = 'anyvalue12'
+  const message =
+    'please check your email account to verify your email and continue the registration process.'
+
+  try {
+    const data = await AuthService.signIn(req)
+    expect(data).toBeTruthy()
+  } catch (error) {
+    expect(error.message).toEqual(message)
+  }
   done()
 })
